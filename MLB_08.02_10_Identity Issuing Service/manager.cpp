70aee68f10c687a61a3ccba472861c854dc86f1a
IT21157882 <100125038+IT21157882@users.noreply.github.com>
2022-05-20 23:06:31 +0530

    create manager cpp file

---
+++ MLB_08.02_10_Identity Issuing Service/manager.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "manager.h"
+
+
+
+manager::manager()
+{
+  cout << "Manager Details :- "<< endl;
+}
+
+manager::manager(int mid, string mname, string mnic, string mdep)
+{
+	Manager_ID = mid;
+	Manager_Name = mname;
+	Manager_Nic = mnic;
+	Manager_Department = mdep;
+
+}
+
+void manager::Display_Manager_Details()
+{
+   cout << "Manager_ID :- "<< Manager_ID  << endl;
+  cout << "Manager_Name :- "<< Manager_Name  << endl;
+  cout << "Manager_Nic :- "<< Manager_Nic  << endl;
+  cout << "Manager_Department :- "<<Manager_Department << endl;
+}
+
+float manager::Calculate_Salary()
+{
+    int salary = Basic_salary + bonus;
+    return salary;
+}
